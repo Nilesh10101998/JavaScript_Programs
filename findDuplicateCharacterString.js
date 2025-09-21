d9b4a8d986dcea23f69272a38334ccb9af72d0a9
Nilesh Kasane <nileshkasane98@gmail.com>
2025-09-21 17:33:18 +0530

    Find duplicate character in the String

---
+++ findDuplicateCharacterString.js
@@ -0,0 +1,33 @@
+
+function findDuplicateCharacters(str) {
+
+    let seen = {};
+    let duplicates = [];
+
+    str = str.toLowerCase();
+
+    for (let i = 0; i < str.length; i++) {
+
+        let char = str[i];
+
+        if (char >= 'a' && char <= 'z') {
+
+            if (seen[char]) {
+
+                if (!duplicates.includes(char)) {
+                    duplicates.push(char);
+                }
+                
+            }
+
+            else {
+                seen[char] = true;
+            }
+        }
+    }
+
+    return duplicates;
+
+}
+
+console.log(findDuplicateCharacters("Cricket"));
